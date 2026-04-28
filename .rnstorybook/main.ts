0f4aaa47834007f6ae0da4850d912f15ebc04478
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2026-04-28 09:36:11 +0100

    Production build changes

--- .rnstorybook/main.ts
+++ .rnstorybook/main.ts
@@ -3,11 +3,11 @@ import type { StorybookConfig } from "@storybook/react-native";
 const main: StorybookConfig = {
   stories: ["../components/**/*.stories.?(ts|tsx|js|jsx)"],
   // Required for Chromatic
-  //addons: [],
-  addons: [
+  addons: [],
+  /*  addons: [
     "@storybook/addon-ondevice-controls",
     "@storybook/addon-ondevice-actions",
-  ],
+  ], */
   // To follow up with the team about this feature
   /* features: {
     ondeviceBackgrounds: true,
